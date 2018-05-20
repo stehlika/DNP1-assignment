49356c59f2682ffbf1ef657395badb6777783093
Adam Stehlik <stehlikadam@gmail.com>
2018-05-20 14:45:00 +0200

    Add project files.

---
+++ DNP1 assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DNP1_assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
